8ec364a05cb3be707e988ac316029cfc3b818d51
meongj <meongjiee@gmail.com>
2026-01-31 21:31:46 +0900

    docs : 2025년 회고글 포스팅

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    // 마크다운 테마
-    markdown: {
-        shikiConfig: {
-            theme: 'catppuccin-frappe',
-            // wrap: true, // 코드 줄 자동 줄바꿈
-        },
+  // 마크다운 테마
+  markdown: {
+    shikiConfig: {
+      theme: 'catppuccin-frappe',
+      // wrap: true, // 코드 줄 자동 줄바꿈
     },
-    site: 'https://meongdee.dev',
-    integrations: [sitemap()],
+  },
+  site: 'https://meongdee.dev',
+  integrations: [sitemap()],
 });
